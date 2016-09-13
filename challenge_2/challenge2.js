2692c8cefc87860c6163070ae628515572483f18
rocheeli <bethsroche@gmail.com>
2016-09-13 16:56:48 -0400

    almost done

--- challenge_2/challenge2.js
+++ challenge_2/challenge2.js
@@ -1,5 +1,27 @@
 $(function(){
 
-	//all your code should go here
+	var control = 0;
+
+	$('h1').click(function() {
+	if(control ==0){
+    $('h1').css('color' ,'red');
+    control++;
+}else{
+	$('h1').css('color' ,'black');
+	control--;
+}
+
+});
+	$('.title:even').hide();
+	$('footer li:even').hide();
+
+	$('aside').remove();
+
+
+$('p').each(function() {
+    var text = $(this).text();
+    $(this).text(text.replace('Bacon', 'Laser Vision')); 
+});
+
 
 })
